76d1185944273a90953e26342302bcbe47678285
Felix Bachmann <felix.bachmann@ewetel.net>
2018-02-20 18:25:42 +0100

    added some constants, added spawn points

--- src/main/java/bomberman/model/StandardArena.java
+++ src/main/java/bomberman/model/StandardArena.java
@@ -1,13 +1,22 @@
 package bomberman.model;
 
 public class StandardArena implements ArenaI {
+	private final int MAX_PLAYERS = 2;
+
 	private int size;
 	private AbstractTile[][] currentMap;
 
+	private final Point[] spawnPoints = new Point[MAX_PLAYERS];
+
 	public StandardArena(int size) {
+		if (size < 4)
+			throw new IllegalArgumentException(
+					"Arena has to have a size of 4 at least, otherwise it would be quite boring.");
 		this.size = size;
 		this.currentMap = new AbstractTile[size][size];
 		this.initArena();
+		this.spawnPoints[0] = new Point(1, 1);
+		this.spawnPoints[1] = new Point(size - 1, size - 1);
 	}
 
 	private void initArena() {
@@ -16,10 +25,14 @@ public class StandardArena implements ArenaI {
 				if (x == 0 || y == 0 || x == (size - 1) || y == (size - 1)) {
 					currentMap[x][y] = UndestroyableTile.getInstance();
 				} else {
-					if (Math.random() > 0.25d) {
+					if ((x == 1 && y == 1) || (x == (size - 2) && y == (size - 2))) {
 						currentMap[x][y] = EmptyTile.getInstance();
 					} else {
-						currentMap[x][y] = DestroyableTile.getInstance();
+						if (Math.random() > 0.25d) {
+							currentMap[x][y] = EmptyTile.getInstance();
+						} else {
+							currentMap[x][y] = DestroyableTile.getInstance();
+						}
 					}
 				}
 			}
@@ -37,7 +50,7 @@ public class StandardArena implements ArenaI {
 
 	@Override
 	public int getMaxPlayers() {
-		return 2;
+		return MAX_PLAYERS;
 	}
 
 }
