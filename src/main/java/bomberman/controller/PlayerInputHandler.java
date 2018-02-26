b45d7f1801ccb04d7b3a15bd61333f889bd198a4
Felix Bachmann <felix.bachmann@ewetel.net>
2018-02-26 16:19:52 +0100

    added PlayerInputHandler comments

--- src/main/java/bomberman/controller/PlayerInputHandler.java
+++ src/main/java/bomberman/controller/PlayerInputHandler.java
@@ -8,6 +8,12 @@ import java.util.Set;
 
 import bomberman.model.Player;
 
+/**
+ * This class handles the input a player may do.
+ * 
+ * @author Felix Bachmann
+ *
+ */
 public class PlayerInputHandler implements KeyListener {
 	private HashMap<Integer, Boolean> isPressed = new HashMap<Integer, Boolean>();
 	private Player player;
@@ -16,7 +22,22 @@ public class PlayerInputHandler implements KeyListener {
 	private final int keyCodeUp;
 	private final int keyCodeDown;
 
-	public PlayerInputHandler(Player player, int keyCodeLeft, int keyCodeRight, int keyCodeUp, int keyCodeDown) {
+	/**
+	 * Creates a new input handler.
+	 * 
+	 * @param player
+	 *            the player to which the input will be redirected
+	 * @param keyCodeLeft
+	 *            the keyCode constant which is used for moving left
+	 * @param keyCodeRight
+	 *            the keyCode constant which is used for moving right
+	 * @param keyCodeUp
+	 *            the keyCode constant which is used for moving up
+	 * @param keyCodeDown
+	 *            the keyCode constant which is used for moving down
+	 */
+	public PlayerInputHandler(Player player, final int keyCodeLeft, final int keyCodeRight, final int keyCodeUp,
+			final int keyCodeDown) {
 		this.player = player;
 		this.keyCodeLeft = keyCodeLeft;
 		this.keyCodeRight = keyCodeRight;
@@ -24,6 +45,9 @@ public class PlayerInputHandler implements KeyListener {
 		this.keyCodeDown = keyCodeDown;
 	}
 
+	/**
+	 * Invokes movement for hold keys.
+	 */
 	public void updateFromPressedKeys() {
 		Set<Integer> keys = isPressed.keySet();
 		Iterator<Integer> iter = keys.iterator();
@@ -35,12 +59,24 @@ public class PlayerInputHandler implements KeyListener {
 		}
 	}
 
+	/**
+	 * Invoked when a key is pressed. This key is added to the pressed keys,
+	 * i.e. until the key is released movement may be invoked.
+	 */
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
 		isPressed.put(arg0.getKeyCode(), true);
 		invokeMovement(keyCode);
 	}
 
+	/**
+	 * Invokes the movement that may be triggered by pressing the button with a
+	 * keyCode. Just does something if the keyCode matches one of the keyCodes
+	 * handed over for this PlayerInputHandler inside the constructor.
+	 * 
+	 * @param keyCode
+	 *            the keyCode of the key which has been pressed
+	 */
 	private void invokeMovement(int keyCode) {
 		if (keyCode == keyCodeLeft) {
 			System.out.println("Player " + player.getId() + ": Try to move left.");
@@ -59,6 +95,10 @@ public class PlayerInputHandler implements KeyListener {
 		}
 	}
 
+	/**
+	 * Invoked when a key is released. This key is removed from the pressed
+	 * keys, i.e. no more movement for this key is invoked until pressed again.
+	 */
 	public void keyReleased(KeyEvent arg0) {
 		isPressed.put(arg0.getKeyCode(), false);
 	}
